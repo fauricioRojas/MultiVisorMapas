74d03b5662bc04c107fb1363f95b9bb7895b4bb7
Kenneth Pérez Alfaro <kenn.perez@gmail.com>
2016-01-12 14:22:59 -0600

    schemas

--- image/imagen.php
+++ image/imagen.php
@@ -4,9 +4,9 @@
     
     //http://localhost:8000/image/imagen.php?type=POINT&strconn=host=localhost%20port=5432%20dbname=cursoGIS%20user=postgres%20password=12345&schema=public&table=distritos&column=centroide&srid=5367&x=640&y=480&r=23&g=23&b=43&trans=10&zoom=0&despX=0&despY=0
     
-    //http://localhost:8000/image/imagen.php?type=MULTIPOLYGON&strconn=host=localhost%20port=5432%20dbname=cursoGIS%20user=postgres%20password=12345schema=public&table=distritos&column=geom&srid=5367&x=640&y=480&r=23&g=23&b=43&trans=10&zoom=0&despX=0&despY=0
+    //http://localhost:8000/image/imagen.php?type=MULTIPOLYGON&strconn=host=localhost%20port=5432%20dbname=cursoGIS%20user=postgres%20password=12345&schema=public&table=distritos&column=geom&srid=5367&x=640&y=480&r=23&g=23&b=43&trans=10&zoom=0&despX=0&despY=0
     
-    //http://localhost:8000/image/imagen.php?type=MULTILINESTRING&strconn=host=localhost%20port=5432%20dbname=cursoGIS%20user=postgres%20password=12345schema=public&table=rios&column=geom&srid=5367&x=640&y=480&r=23&g=23&b=43&trans=10&zoom=0&despX=0&despY=0
+    //http://localhost:8000/image/imagen.php?type=MULTILINESTRING&strconn=host=localhost%20port=5432%20dbname=cursoGIS%20user=postgres%20password=12345&schema=public&table=rios&column=geom&srid=5367&x=640&y=480&r=23&g=23&b=43&trans=10&zoom=0&despX=0&despY=0
  
     
     $type = $_REQUEST['type'];
